43cf8ef107d41e1d85c3f780b888b0d385107c20
Imikeeee <38005932+Imikeeee@users.noreply.github.com>
2023-06-12 16:26:01 +0200

    Add files via upload

--- Kutyak/Program.cs
+++ Kutyak/Program.cs
@@ -1,4 +1,4 @@
-﻿using System;
+using System;
 using System.IO;
 using System.Linq;
 using System.Collections.Generic;
@@ -12,131 +12,166 @@ namespace Kutyak
         public static List<Kutyak> kutyak = new List<Kutyak>();
         static void Main(string[] args)
         {
-            beolvas();
-            Console.WriteLine("3. feladat: "+kutyanevek.Count()) ;
-            Console.WriteLine("6. feladat: A kutyák átlagéletkora: " + atlag().ToString("0.##"));
-            hetedik();
-            nyolcadik();
-            kilencedik();
-            tiz();
-            Console.ReadKey();
+            try
+            {
+                beolvas();
+                Console.WriteLine("3. feladat: " + kutyanevek.Count());
+                Console.WriteLine("6. feladat: A kutyák átlagéletkora: " + atlag().ToString("0.##"));
+                hetedik();
+                nyolcadik();
+                kilencedik();
+                tiz();
+            }
+            catch (FileNotFoundException ex)
+            {
+                Console.WriteLine("One or more CSV files not found: " + ex.Message);
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+            finally
+            {
+                Console.ReadKey();
+            }
         }
 
         public static void beolvas()
         {
-            
-            foreach (var item in File.ReadAllLines("KutyaNevek.csv", System.Text.Encoding.UTF8).Skip(1))
+            string kutyaNevekFile = "KutyaNevek.csv";
+            string kutyaFajtakFile = "KutyaFajtak.csv";
+            string kutyakFile = "Kutyak.csv";
+
+            if (!File.Exists(kutyaNevekFile) || !File.Exists(kutyaFajtakFile) || !File.Exists(kutyakFile))
+            {
+                throw new FileNotFoundException("One or more CSV files not found.");
+            }
+
+            foreach (var item in File.ReadAllLines(kutyaNevekFile, System.Text.Encoding.UTF8).Skip(1))
             {
                 KutyaNevek kutya = new KutyaNevek(item);
                 kutyanevek.Add(kutya);
             }
 
-            foreach (var item in File.ReadAllLines("KutyaFajtak.csv", System.Text.Encoding.UTF8).Skip(1))
+            foreach (var item in File.ReadAllLines(kutyaFajtakFile, System.Text.Encoding.UTF8).Skip(1))
             {
                 KutyaFajtak kutya = new KutyaFajtak(item);
                 kutyafajtak.Add(kutya);
             }
 
-            foreach (var item in File.ReadAllLines("Kutyak.csv", System.Text.Encoding.UTF8).Skip(1))
+            foreach (var item in File.ReadAllLines(kutyakFile, System.Text.Encoding.UTF8).Skip(1))
             {
                 Kutyak kutya = new Kutyak(item);
                 kutyak.Add(kutya);
             }
         }
+
         public static decimal atlag()
         {
-            /* LINQ-es sum helyett hagyományos megoldás az összegzésre
-            decimal total = 0;
-            foreach (var item in kutyak)
-            { total += item.kor;}        */
-            decimal atlag = 0;
-            var total = kutyak.Sum(a => a.kor);
-            atlag =(decimal) total / (decimal)kutyak.Count();
+            if (kutyak.Count == 0)
+            {
+                return 0;
+            }
+
+            decimal atlag = kutyak.Average(a => a.kor);
             return atlag;
         }
 
         public static void hetedik()
         {
-            /* hagyományos megoldás
-            int maxkor = kutyak[0].kor;
-            string neve = "", fajtaja = "";
-            foreach (var item in kutyak)
+            if (kutyak.Count == 0)
             {
-                if (item.kor > maxkor )
-                {
-                    maxkor = item.kor;
-                    neve = kutyanevek.Where(a => a.azon == item.nevazon).FirstOrDefault().nev;   //itt is további foreach kellene LINQ nélkül :(
-                    fajtaja = kutyafajtak.Where(a => a.id == item.fajtaazon).FirstOrDefault().nev;
-                }
+                Console.WriteLine("7. Feladat: Nincs kutya az adatbázisban.");
+                return;
             }
-            Console.WriteLine("7. Feladat: A legidősebb kutya neve és fajtája: " + neve + ", " + fajtaja); 
-            */
 
             var kutya = kutyak.OrderByDescending(a => a.kor).First();
-            Console.WriteLine("7. Feladat: A legidősebb kutya neve és fajtája: "+kutyanevek.Where(a=>a.azon==kutya.nevazon).FirstOrDefault().nev+", "+ kutyafajtak.Where(a => a.id == kutya.fajtaazon).FirstOrDefault().nev);
-            
+            Console.WriteLine("7. Feladat: A legidősebb kutya neve és fajtája: " +
+                kutyanevek.Where(a => a.azon == kutya.nevazon).FirstOrDefault()?.nev + ", " +
+                kutyafajtak.Where(a => a.id == kutya.fajtaazon).FirstOrDefault()?.nev);
         }
 
         public static void nyolcadik()
         {
+            DateTime targetDate = DateTime.Parse("2018.01.10");
+            var filteredKutyak = kutyak.Where(a => a.ideje == targetDate);
+
+            if (filteredKutyak.Count() == 0)
+            {
+                Console.WriteLine("8. feladat: Nincs kutya az adott napon.");
+                return;
+            }
 
             Dictionary<string, int> fajtak = new Dictionary<string, int>();
-            foreach (Kutyak kutya in kutyak)
+            foreach (Kutyak kutya in filteredKutyak)
             {
-                if (kutya.ideje == DateTime.Parse("2018.01.10"))
-                {
-                    string key = kutyafajtak.Where(a => a.id == kutya.fajtaazon).Single().eredetinev;
+                string key = kutyafajtak.Where(a => a.id == kutya.fajtaazon).Single().eredetinev;
 
-                    if (fajtak.ContainsKey(key))
-                                                 fajtak[key]++;
-                    else
-                        fajtak.Add(key, 1);
-                }         
+                if (fajtak.ContainsKey(key))
+                    fajtak[key]++;
+                else
+                    fajtak.Add(key, 1);
             }
+
             Console.WriteLine("8. feladat:");
             foreach (var item in fajtak)
             {
-                Console.WriteLine("\t "+item.Key+": "+item.Value+" kutya");
+                Console.WriteLine("\t " + item.Key + ": " + item.Value + " kutya");
             }
-
         }
+
         public static void kilencedik()
         {
+            if (kutyak.Count == 0)
+            {
+                Console.WriteLine("9. feladat: Nincs kutya az adatbázisban.");
+                return;
+            }
+
             Dictionary<string, int> napok = new Dictionary<string, int>();
             foreach (Kutyak kutya in kutyak)
             {
                 string key = kutya.ideje.ToString("yyyy-MM-dd");
 
-                    if (napok.ContainsKey(key))
+                if (napok.ContainsKey(key))
                     napok[key]++;
-                    else
+                else
                     napok.Add(key, 1);
             }
-            Console.WriteLine("9. feladat: Legjobban leterhelt nap: "+ napok.FirstOrDefault(x => x.Value == napok.Values.Max()).Key+ ": "+napok.Values.Max()+" kutya"); 
 
+            var maxNap = napok.OrderByDescending(x => x.Value).FirstOrDefault();
+            Console.WriteLine("9. feladat: Legjobban leterhelt nap: " + maxNap.Key + ": " + maxNap.Value + " kutya");
         }
 
         public static void tiz()
         {
-            using (StreamWriter sw = new StreamWriter("Névstatisztika.txt",false, System.Text.Encoding.UTF8))
+            if (kutyak.Count == 0)
             {
-                Dictionary<string, int> nevek = new Dictionary<string, int>();
-                foreach (Kutyak kutya in kutyak)
-                {
-                    string key = kutyanevek.Where(a => a.azon == kutya.nevazon).Single().nev;
+                Console.WriteLine("10. feladat: Nincs kutya az adatbázisban.");
+                return;
+            }
 
-                    if (nevek.ContainsKey(key))
-                        nevek[key]++;
-                    else
-                        nevek.Add(key, 1);
-                }
-               var sortednevek = from entry in nevek orderby entry.Value descending select entry;
+            Dictionary<string, int> nevek = new Dictionary<string, int>();
+            foreach (Kutyak kutya in kutyak)
+            {
+                string key = kutyanevek.Where(a => a.azon == kutya.nevazon).Single().nev;
+
+                if (nevek.ContainsKey(key))
+                    nevek[key]++;
+                else
+                    nevek.Add(key, 1);
+            }
+
+            var sortednevek = from entry in nevek orderby entry.Value descending select entry;
+
+            using (StreamWriter sw = new StreamWriter("Névstatisztika.txt", false, System.Text.Encoding.UTF8))
+            {
                 foreach (var item in sortednevek)
                 {
                     sw.WriteLine(item.Key + ";" + item.Value);
                 }
-         
             }
+
             Console.WriteLine("10. feladat: névstatisztika.txt");
         }
     }
